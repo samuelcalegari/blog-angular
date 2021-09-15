35b93b590a86b294ef147b248d0159e719b97b7d
miro <samuel.calegari@univ-perp.fr>
2021-09-15 10:40:40 +0200

    init

---
+++ src/app/post-list-item-component/post-list-item-component.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item-component',
+  templateUrl: './post-list-item-component.component.html',
+  styleUrls: ['./post-list-item-component.component.scss']
+})
+export class PostListItemComponentComponent implements OnInit {
+
+  @Input() post : any
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

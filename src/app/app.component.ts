463261cbf173584a12b079624dfff934d5a3c635
miro <samuel.calegari@univ-perp.fr>
2021-09-15 10:40:13 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+
+  posts = [{
+    title : 'Mon premier post',
+    content : 'bla bla',
+    loveIts : 1,
+  },{
+    title : 'Mon second post',
+    content : 'bla bla bla',
+    loveIts : 3,
+  },{
+    title : 'Encore un post',
+    content : 'pour finir ...',
+    loveIts : 0,
+  }];
 }
